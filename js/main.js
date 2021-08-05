40d6277ad520c3483b96dfc75a1161e67d793184
SofiaHritcu <sofia.hritcu@principal33.com>
2021-08-05 12:05:20 +0300

    #CMS - add: adding employee rows to the table

--- js/main.js
+++ js/main.js
@@ -1,56 +1,55 @@
-var data = [
-    {
-        name: "bootstrap-table",
-        stargazers_count: "526",
-        forks_count: "122",
-        description: "An extended Bootstrap table with radio, checkbox, sort, pagination, and other added features. (supports twitter bootstrap v2 and v3) "
-    },
-    {
-        name: "multiple-select",
-        stargazers_count: "288",
-        forks_count: "150",
-        description: "A jQuery plugin to select multiple elements with checkboxes :)"
-    },
-    {
-        name: "bootstrap-show-password",
-        stargazers_count: "32",
-        forks_count: "11",
-        description: "Show/hide password plugin for twitter bootstrap."
-    },
-    {
-        name: "blog",
-        stargazers_count: "13",
-        forks_count: "4",
-        description: "my blog"
-    },
-    {
-        name: "scutech-redmine",
-        stargazers_count: "6",
-        forks_count: "3",
-        description: "Redmine notification tools for chrome extension."
-    },
-    {
-        name: "scutech-redmine1",
-        stargazers_count: "6",
-        forks_count: "3",
-        description: "Redmine notification tools for chrome extension."
-    }
-];
 
-function nameFormatter(value) {
-    return '<a href="https://github.com/wenzhixin/' + value + '">' + value + '</a>';
+var id = "1";
+var firstName = "firstName";
+var lastName = "lastName";
+var email ="email";
+var gender = "gender";
+
+
+
+
+
+
+function createTd( text ){
+    td =  document.createElement('td');
+    var tdText = document.createTextNode(text);
+    td.appendChild(tdText);
+    return td;
 }
 
-function starsFormatter(value) {
-    return '<i class="glyphicon glyphicon-star"></i> ' + value;
+
+
+function addTableInstance (id, firstName, lastName, email, gender) {
+    var tr = document.createElement('tr');
+
+    var tdId = createTd(id);
+    tdId.setAttribute("scope","row");
+
+    var tdFirstName = createTd(firstName);
+    var tdLastName = createTd(lastName);
+    var tdEmail = createTd(email);
+    var tdGender = createTd(gender);
+
+    // var tdFirstName = document.createElement('td');
+    // var tdEmail = document.createElement('td');
+    // var tdGender = document.createElement('td');
+    // var textId = document.createTextNode(id);
+    // var textFirstName = document.createTextNode(firstName);
+    // var textLastName = document.createTextNode(lastName);
+    // var textEmail = document.createTextNode(email);
+    // var textGender = document.createTextNode(gender);
+    // tdId.appendChild(textId);
+    // tdFirstName.appendChild(textFirstName);
+    // tdLastName.appendChild(textLastName);
+    // tdEmail.appendChild(textEmail);
+    // tdGender.appendChild(textGender);
+
+    tr.appendChild(tdId);
+    tr.appendChild(tdFirstName);
+    tr.appendChild(tdLastName);
+    tr.appendChild(tdEmail);
+    tr.appendChild(tdGender);
+    document.querySelector("tbody").appendChild(tr);
 }
 
-function forksFormatter(value) {
-    return '<i class="glyphicon glyphicon-cutlery"></i> ' + value;
-}
-
-$(function () {
-    $('#table').bootstrapTable({
-        data: data
-    });
-});
+addTableInstance(id, firstName, lastName, email, gender);
